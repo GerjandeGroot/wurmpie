f37e88c39ea69fff27b25700e0719be897c9a321
Wibo <wibo.wester@live.nl>
2018-12-20 12:09:07 +0100

    deleted own files

--- Arduino/teste/Functies.h.ino
+++
@@ -1,8 +0,0 @@
-void Trein() {
-  int afstand_trein    = 0;                                           // huidige locatie
-  int detectie_trein_1 = 0;                                           // op ref sensor
-  int detectie_trein_2 = 0;                                           // afstand al verhoogd
-  int eindpunt_trein1  = 0;                                           // gekozen eindpunt
-  int klaar_trein      = 0;
-}
-
